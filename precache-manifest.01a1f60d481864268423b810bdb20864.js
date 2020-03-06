8e153791ee71f537210db786bd9020b9a022dd22
Oleh-Lee <rrrgo3@gmail.com>
2020-03-06 15:13:47 +0200

    Updates

---
+++ precache-manifest.01a1f60d481864268423b810bdb20864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13b343c7758d70ee83f851ab861ab9f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3982efcc75d9fedca364",
+    "url": "/goit-react-hw-01-components/static/css/main.e4331f53.chunk.css"
+  },
+  {
+    "revision": "65b5e24c9dc450d9e576",
+    "url": "/goit-react-hw-01-components/static/js/2.c0b3fe9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.c0b3fe9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3982efcc75d9fedca364",
+    "url": "/goit-react-hw-01-components/static/js/main.5075100e.chunk.js"
+  },
+  {
+    "revision": "a62329f2cc0b24bc37f9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.39b5137b.js"
+  }
+]);
